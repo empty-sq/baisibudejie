df265dfc66c0a443aee13e5d1942789393613a63
SQ <865329347@qq.com>
2016-03-24 22:51:23 +0800

    初始化

---
+++ baisibudejie/baisibudejie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baisibudejie
+//
+//  Created by 沈强 on 16/3/24.
+//  Copyright © 2016年 SQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ baisibudejie/baisibudejie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  baisibudejie
+//
+//  Created by 沈强 on 16/3/24.
+//  Copyright © 2016年 SQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
